d5b7ed4858fd686ef8960865980bd1f9e6146ec0
Vladislav Grishkevich <vladislav.grishkevich@gmail.com>
2021-02-22 05:36:35 +0300

    Bind Title property to ViewModel

--- CP_2021/ViewModels/Base/ViewModelBase.cs
+++ CP_2021/ViewModels/Base/ViewModelBase.cs
@@ -1,8 +1,9 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System;
 namespace CP_2021.ViewModels.Base
 {
-    internal abstract class ViewModelBase : INotifyPropertyChanged
+    internal abstract class ViewModelBase : INotifyPropertyChanged/*, IDisposable*/
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -18,5 +19,18 @@ namespace CP_2021.ViewModels.Base
             OnPropertyChanged(PropertyName);
             return true;
         }
+
+        /*public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        private bool _Disposed;
+
+        protected virtual void Dispose(bool Disposing)
+        {
+            if (!Disposing || _Disposed) return;
+            _Disposed = true;
+        }*/
     }
 }
---
+++ CP_2021/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using CP_2021.ViewModels.Base;
+namespace CP_2021.ViewModels
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+        #region Title
+        private string _Title = "Main Window";
+
+        /// <summary>Заголовок окна</summary>
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+        #endregion
+    }
+}
