af2f834dca02029886b8d4d78609bc33fa23f7e9
Vladislav Grishkevich <vladislav.grishkevich@gmail.com>
2021-07-31 19:54:26 +0300

    Исправлена ошибка с поднятием задачи из Model

--- CP_2021/ViewModels/ProductionPlanControlViewModel.cs
+++ CP_2021/ViewModels/ProductionPlanControlViewModel.cs
@@ -612,7 +612,7 @@ namespace CP_2021.ViewModels
             if(parent == null)
             {
                 task = Unit.Tasks.Get().Where(t => t.MyParent.Parent == null && t.MyParent.LineOrder == selectedOrderBase - 1).SingleOrDefault();
-                Model.SwapItems(selectedOrderBase - 1, selectedOrderBase - 2);
+                //Model.SwapItems(selectedOrderBase - 1, selectedOrderBase - 2);
             }
             else
             {
@@ -642,6 +642,28 @@ namespace CP_2021.ViewModels
 
                 parent.Children.Insert(selectedOrderBase - 2, taskToUp);
                 parent.Children.Insert(selectedOrderBase - 1, taskToDown);
+            }
+            else
+            {
+                var taskToUp = SelectedTask.Clone();
+                Model.Remove(SelectedTask);
+
+                ProductionTask taskToDown = new ProductionTask();
+                foreach(ProductionTask root in Model)
+                {
+                    if (root.Task.Equals(task))
+                    {
+                        taskToDown = root.Clone();
+                        Model.Remove(root);
+                        break;
+                    }
+                }
+
+                taskToDown.IsExpanded = false;
+                taskToUp.IsExpanded = false;
+
+                Model.Insert(selectedOrderBase - 2, taskToUp);
+                Model.Insert(selectedOrderBase - 1, taskToDown);
             }
             Unit.Commit();
         }
